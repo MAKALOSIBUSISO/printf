a113f3251b393328c8dbdf9c95a86545c8882883
kudzanai92 <keezentpty@gmail.com>
2023-03-27 08:36:09 -0700

    understanding flags

--- get_flags.c
+++ get_flags.c
@@ -1,34 +1,34 @@
 #include "main.h"
 /**
  * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
+ * @format: Formatted string print the arguments
+ * @i: parameter to be taken.
  * Return: Flags:
  */
 int get_flags(const char *format, int *i)
 {
-	int j, curr_i;
+	int j, kr_i;
 	int flags = 0;
-	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+	const char flg_ch[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int flg_arr[] = {Flg_minus, Flg_plus, Flg_zero, Flg_hash, Flg_space, 0};
 
-	curr_i = *i + 1;
-	while (format[curr_i] != '\0')
+	kr_i = *i + 1;
+	while (format[kr_i] != '\0')
 	{
 		j = 0;
-		while (FLAGS_CH[j] != '\0')
+		while (flg_ch[j] != '\0')
 		{
-			if (format[curr_i] == FLAGS_CH[j])
+			if (format[kr_i] == flg_ch[j])
 			{
-				flags |= FLAGS_ARR[j];
+				flags |= flg_arr[j];
 				break;
 			}
 			j++;
 		}
-		if (FLAGS_CH[j] == 0)
+		if (flg_ch[j] == 0)
 			break;
-		curr_i++;
+		kr_i++;
 	}
-	*i = curr_i - 1;
+	*i = kr_i - 1;
 	return (flags);
 }
