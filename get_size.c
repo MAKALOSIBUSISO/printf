8dd25058342f256887f28a7b30aae4b9abb7d0ef
kudzanai92 <keezentpty@gmail.com>
2023-04-01 21:24:35 -0700

    /**

--- get_size.c
+++ get_size.c
@@ -9,18 +9,37 @@
  */
 int get_size(const char *format, int *i)
 {
-	int curr_i = *i + 1;
+	int curr_i = (*i) + 1;
 	int size = 0;
 
-	if (format[curr_i] == 'l')
-		size = S_LONG;
-	else if (format[curr_i] == 'h')
-		size = S_SHORT;
-
-	if (size == 0)
-		*i = curr_i - 1;
-	else
-		*i = curr_i;
+	switch (format[curr_i])
+	{
+		case 'l':
+			size = S_LONG;
+			break;
+		case 'h':
+			size = S_SHORT;
+			break;
+		default:
+			*i = curr_i - 1;
+			break;
+	}
+	*i = size ? curr_i : *i;
 
 	return (size);
+}
+
+/**
+ * main - function that executes the program.
+ *
+ * Return: 0 if the program exits successfully.
+ */
+int main(void)
+{
+	const char *format = "l";
+	int i = 0;
+	int size = get_size(format, &i);
+
+	printf("%d\n", size);
+	return (0);
 }
